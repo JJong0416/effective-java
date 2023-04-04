191f16ccc4c07fdaaa9f54b66385a60cbf285cda
jjong0416 <whdcks5681@naver.com>
2023-04-04 18:40:43 +0900

    3장 Item14 Code & Readme

---
+++ src/letcure03/Item14/WordList.java
@@ -0,0 +1,22 @@
+package letcure03.Item14;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class WordList {
+    public static void main(String[] args) {
+        Set<String> s = new TreeSet<>();
+        Collections.addAll(s, args);
+        System.out.println(s);
+    }
+
+    public int AntiCompareTo(int x, int y) {
+        return x < y ? 1 : (x == y) ? 0 : -1;
+    }
+
+    static Comparator<Object> hashCodeOrder =
+            Comparator.comparingInt(o -> o.hashCode());
+
+}
